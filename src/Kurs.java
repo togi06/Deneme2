6ce581cd305e8b89693a6efbfef0f3e1eb2c529c
togi06 <t_bayter_06@hotmail.com>
2022-12-12 21:16:43 +0100

    ı have done my job

---
+++ src/Kurs.java
@@ -0,0 +1,15 @@
+public class Kurs {
+
+
+    public static void main(String[] args) {
+        int n= 4;
+        if (n<5){
+            System.out.println("Kurs Kolay");
+        }else{
+            System.out.println("Kurs Zor");
+        }
+    }
+
+
+
+}
